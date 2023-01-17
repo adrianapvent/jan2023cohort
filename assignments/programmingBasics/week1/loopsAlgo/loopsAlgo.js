137e9b9b0f4815b717c4cd60a775c0b18010292d
adrianapvent <adriana.e.pena@gmail.com>
2023-01-17 17:39:46 -0600

    2 challenge questions completed

--- assignments/programmingBasics/week1/loopsAlgo/loopsAlgo.js
+++ assignments/programmingBasics/week1/loopsAlgo/loopsAlgo.js
@@ -1,6 +1,18 @@
-for(var num = 1; num <= 20; num++) {
-    if(num % 2 == 1){
-        console.log('Practice 1 AP:', num)
-    }
+for (var num = 1; num <= 20; num++) {
+	if (num % 2 == 1) {
+		console.log('Practice 1 AP:', num)
+	}
 }
-// results will present with odd numbers
+// Challenge 1 - results will present with odd numbers under 20
+
+var sum = 0;
+for (var num = 1; num <= 5; num++) {
+	sum = sum + num;
+	console.log(
+		'Practice 2 AP: current value of num is:',
+		num,
+		'current sum is:',
+		sum
+	)
+}
+// Challenge 2 is able to print the current number and add to the sum after each loop
