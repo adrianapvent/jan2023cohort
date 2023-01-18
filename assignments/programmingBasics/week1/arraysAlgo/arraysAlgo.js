a844abd5b7f1666567cab94819ab1e118f1b21e5
adrianapvent <adriana.e.pena@gmail.com>
2023-01-18 17:42:53 -0600

    current updates

---
+++ assignments/programmingBasics/week1/arraysAlgo/arraysAlgo.js
@@ -0,0 +1,11 @@
+var testArr = [6,3,5,1,2,4];
+// for (var num = i; i < arr.length; i++){
+//         console.log ("Value:", num)
+//         if {
+//             var sum = 0,
+//             sum = sum + newTestArr;
+//         }
+//         console.log("Sum:", sum)
+// }
+
+
