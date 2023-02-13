7bb04dfef107d4261965d92d13631998f3f5c23e
adrianapvent <adriana.e.pena@gmail.com>
2023-02-13 09:13:48 -0600

    playing with loops

--- assignments/programmingBasics/week2/foundation1.js
+++ assignments/programmingBasics/week2/foundation1.js
@@ -185,4 +185,24 @@ function swapDojo (arr) {
     console.log(arr) // console new array
 }
 
-swapDojo ([-1,-2,3])
+// swapDojo ([-1,-2,3])
+
+// console.log( 8 % 3 );
+
+// console.log( 8 % 3 == 2 );
+
+// for(var i=8; i > -2; i-=3) {
+//     console.log(i);
+// }
+
+var x = 0;
+for(var i=1; i<5; i++) {
+    x += i;
+}
+console.log(x);
+
+// var x = "0";
+// for(var i=1; i<5; i++) {
+//      x += i;
+//  }
+//  console.log(x);
