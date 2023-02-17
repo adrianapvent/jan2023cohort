f32a245fcc05e53351aa9e530e46bed9de408b98
adrianapvent <adriana.e.pena@gmail.com>
2023-02-16 18:29:16 -0600

    fundamentals 3 progress

--- assignments/cSharp/Fundamentals3/Program.cs
+++ assignments/cSharp/Fundamentals3/Program.cs
@@ -75,31 +75,48 @@ foreach (int num in SquareValues(TestIntList3))
 {
     Console.WriteLine(num + " ");
 }
-// // 5. Replace Negative Numbers with 0
-// static int[] NonNegatives(int[] IntArray)
-// {
-//     // Your code here
-// }
-// int[] TestIntArray = new int[] {-1,2,3,-4,5};
-// // You should get back [0,2,3,0,5], think about how you will show that this worked
-// NonNegatives(TestIntArray);
+Console.WriteLine("------------");
+// 5. Replace Negative Numbers with 0
+static int[] NonNegatives(int[] IntArray)
+{
+    for(int i = 0; i < IntArray.Length; i++) 
+    {
+        if(IntArray[i] < 0) {
+            IntArray[i] = 0;
+        }
+    }
+    return IntArray;
+}
+int[] TestIntArray = new int[] {-1,2,3,-4,5};
+// You should get back [0,2,3,0,5], think about how you will show that this worked
+NonNegatives(TestIntArray);
+foreach (int num in NonNegatives(TestIntArray))
+{
+    Console.WriteLine(num);
+}
+Console.WriteLine("------------");
 
 
-// // 6. Print Dictionary
-// static void PrintDictionary(Dictionary<string,string> MyDictionary)
-// {
-//     // Your code here
-// }
-// Dictionary<string,string> TestDict = new Dictionary<string,string>();
-// TestDict.Add("HeroName", "Iron Man");
-// TestDict.Add("RealName", "Tony Stark");
-// TestDict.Add("Powers", "Money and intelligence");
-// PrintDictionary(TestDict);
+// 6. Print Dictionary
+static void PrintDictionary(Dictionary<string,string> MyDictionary)
+{
+    foreach(KeyValuePair<string, string> item in MyDictionary)
+    {
+        Console.WriteLine($"{item.Key} - {item.Value}");
+    }
+}
+Dictionary<string,string> TestDict = new Dictionary<string,string>();
+TestDict.Add("HeroName", "Iron Man");
+TestDict.Add("RealName", "Tony Stark");
+TestDict.Add("Powers", "Money and intelligence");
+PrintDictionary(TestDict);
+Console.WriteLine("------------");
 
-// // 7. Find Key
+
+// 7. Find Key
 // static bool FindKey(Dictionary<string,string> MyDictionary, string SearchTerm)
 // {
-//     // Your code here
+//     Dictionary<string,string> 
 // }
 // // Use the TestDict from the earlier example or make your own
 // // This should print true
@@ -107,19 +124,32 @@ foreach (int num in SquareValues(TestIntList3))
 // // This should print false
 // Console.WriteLine(FindKey(TestDict, "Name"));
 
-// // 8. Generate a Dictionary
-// // Ex: Given ["Julie", "Harold", "James", "Monica"] and [6,12,7,10], return a dictionary
-// // {
-// //	"Julie": 6,
-// //	"Harold": 12,
-// //	"James": 7,
-// //	"Monica": 10
-// // } 
-// static Dictionary<string,int> GenerateDictionary(List<string> Names, List<int> Numbers)
+ // 8. Generate a Dictionary
+// Ex: Given ["Julie", "Harold", "James", "Monica"] and [6,12,7,10], return a dictionary
 // {
-//     // Your code here
-// }
-// // We've shown several examples of how to set your tests up properly, it's your turn to set it up!
-// // Your test code here
+//	"Julie": 6,
+//	"Harold": 12,
+//	"James": 7,
+//	"Monica": 10
+// } 
+static void GenerateDictionary()
+{
+    Dictionary<string,int> Students = new Dictionary<string, int>();
+
+    Students.Add("Julie", 6);
+    Students.Add("Harold", 12);
+    Students.Add("James", 7);
+    Students.Add("Monica", 10);
+
+    Console.WriteLine("Students");
+
+    foreach(KeyValuePair<string,int> entry in Students)
+    {
+        Console.WriteLine($"{entry.Key} - {entry.Value}");
+    }
+}
+GenerateDictionary();
+// We've shown several examples of how to set your tests up properly, it's your turn to set it up!
+// Your test code here
 
 
