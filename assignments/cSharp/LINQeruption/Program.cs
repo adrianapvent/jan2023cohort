5ffaa43684b449010887d7e211fade9767238bfc
adrianapvent <adriana.e.pena@gmail.com>
2023-03-01 16:02:04 -0600

    LINQ eruption completed

--- assignments/cSharp/LINQeruption/Program.cs
+++ assignments/cSharp/LINQeruption/Program.cs
@@ -23,21 +23,50 @@ List<Eruption> eruptions = new List<Eruption>()
 
 // Execute Assignment Tasks here!
 // 1. Use LINQ to find the first eruption that is in Chile and print the result.
-List<Eruption> namedChile = eruptions.Where(c => c.Location == "Chile").ToList();
-PrintEach(namedChile, "Eruption in Chile");
+// List<Eruption> namedChile = eruptions.Where(c => c.Location == "Chile").ToList();
+// PrintEach(namedChile, "Eruption in Chile");
 
 //2. Find the first eruption from the "Hawaiian Is" location and print it. If none is found, print "No Hawaiian Is Eruption found."
-var hawaiianEruptions = eruptions.FirstOrDefault(c => c.Location == "Hawaiian Is");
-// PrintEach(hawaiianEruptions, "Hawaiian eruptions.");
-if(hawaiianEruptions == null) {
-    Console.WriteLine("No Hawaiian Is Eruption found.");
-} else {
-    Console.WriteLine(hawaiianEruptions);
-}
+// var hawaiianEruptions = eruptions.FirstOrDefault(c => c.Location == "Hawaiian Is");
+// // PrintEach(hawaiianEruptions, "Hawaiian eruptions.");
+// if(hawaiianEruptions == null) {
+//     Console.WriteLine("No Hawaiian Is Eruption found.");
+// } else {
+//     Console.WriteLine(hawaiianEruptions);
+// }
 
 //3. Find the first eruption that is after the year 1900 AND in "New Zealand", then print it.
-var NewZealandEruptions = eruptions.Where(c => c.Locations == "New Zealand").FirstOrDefault(y => y.Year > 1900);
-Console.WriteLine(NewZealandEruptions);
+// var newZealandEruptions = eruptions.Where(c => c.Location == "New Zealand").FirstOrDefault(y => y.Year > 1900);
+// Console.WriteLine(newZealandEruptions);
+
+//5. Find all eruptions where the volcano's elevation is over 2000m and print them.
+// IEnumerable<Eruption> eruptionOver = eruptions.Where(o =>o.ElevationInMeters > 2000);
+// PrintEach(eruptionOver);
+
+//6. Find all eruptions where the volcano's name starts with "L" and print them. Also print the number of eruptions found.
+// IEnumerable<Eruption> lEruptions = eruptions.Where(n => n.Volcano.StartsWith("L")).ToList();
+// PrintEach(lEruptions, "eruptions starting with L");
+// int lEruptionsNum = eruptions.Where(n => n.Volcano.StartsWith("L")).Count();
+// Console.WriteLine(lEruptionsNum);
+
+
+//7. Find the highest elevation, and print only that integer (Hint: Look up how to use LINQ to find the max!)
+// int maxElevation = eruptions.Max(m => m.ElevationInMeters);
+// Console.WriteLine(maxElevation);
+
+// //8. Use the highest elevation variable to find a print the name of the Volcano with that elevation.
+// string highestElevation = eruptions.FirstOrDefault(h => h.ElevationInMeters == maxElevation).Volcano;
+// Console.WriteLine(highestElevation);
+
+//9. Print all Volcano names alphabetically.
+// IEnumerable<Eruption> alphabetically = eruptions.OrderBy(a => a.Volcano);
+// PrintEach(alphabetically);
+
+//10. Print all the eruptions that happened before the year 1000 CE alphabetically according to Volcano name.
+// IEnumerable<Eruption> before1000 = eruptions.Where(e => e.Year < 1000).OrderBy(a => a.Volcano);
+// PrintEach(before1000);
+
+//11. BONUS: Redo the last query, but this time use LINQ to only select the volcano's name so that only the names are printed.
 
 // Helper method to print each item in a List or IEnumerable.This should remain at the bottom of your class!
 static void PrintEach(IEnumerable<dynamic> items, string msg = "")
