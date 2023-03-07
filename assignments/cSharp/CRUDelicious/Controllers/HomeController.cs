1e0f6fbcb242b36fe02934f8d19ca04040bbaba1
adrianapvent <adriana.e.pena@gmail.com>
2023-03-07 12:09:30 -0600

    CRUDelicious working all CRUD items

--- assignments/cSharp/CRUDelicious/Controllers/HomeController.cs
+++ assignments/cSharp/CRUDelicious/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CRUDelicious.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDelicious.Controllers;
 
@@ -8,15 +10,112 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private MyContext db; 
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+        db = context;
     }
 
+    //ViewAll Dishes
+    [HttpGet("")]
     public IActionResult Index()
     {
+        List<Dish> allDishes = db.Dishes.OrderByDescending(d => d.DishId).ToList();
+        // ViewBag.allDishes = allDishes;
+        return View(allDishes);
+        // return View("Index");  
+    }
+
+    //create the dish
+    [HttpGet("/addDish")]
+    public IActionResult AddDish() {
         return View();
     }
+
+    //post the created Dish
+    [HttpPost("/Create")]
+    public IActionResult CreateDish(Dish mydish)
+    {
+        if(ModelState.IsValid)
+        {
+            db.Add(mydish);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View("AddDish");
+    }
+
+    //view 1 dish w/details
+    [HttpGet("Home/addDish/{DishId}")]
+    public IActionResult ViewDish(int DishId)
+    {
+        Dish? item = db.Dishes.FirstOrDefault(i => i.DishId == DishId);
+
+        if(item == null){
+            return RedirectToAction("Index");
+        }
+        else {
+        return View("ViewDish", item);
+        }
+    }
+
+    //get item to edit
+    [HttpGet("/Home/{TestId}/editDish")]
+    public IActionResult EditDish(int TestId)
+    {
+        Console.WriteLine("================");
+        Dish? item = db.Dishes
+        // .Include(item => item.Name)
+            .FirstOrDefault(item => item.DishId == TestId);
+        if(item == null){
+            return RedirectToAction("Index");
+        } else {
+        // Console.WriteLine("================");
+        return View("editDish", item);
+        }
+    }
+    //update item
+    [HttpPost("/Home/updateDish/{TestId}")]
+    public IActionResult Update(Dish newItem, int TestId){
+        Console.WriteLine("What's happening");
+        if(ModelState.IsValid){
+            Dish? item = db.Dishes
+            .FirstOrDefault(item => item.DishId == TestId);
+            if(item == null){
+                Console.WriteLine("failed dish id");
+                return RedirectToAction("Index");
+            } else {
+            item.Name = newItem.Name;
+            item.Chef = newItem.Chef;
+            item.Tastiness = newItem.Tastiness;
+            item.Calories = newItem.Calories;
+            item.Description = newItem.Description;
+            item.UpdatedAt = DateTime.Now;
+            db.Dishes.Update(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+        } else {
+        Console.WriteLine("failed validations");
+        return View("editDish");
+        }
+    }
+
+    //delete item
+    [HttpGet("/Home/{dishId}/Delete")]
+    public IActionResult DeleteDish(int dishId) {
+        Dish? item = db.Dishes
+        .FirstOrDefault(item => item.DishId == dishId);
+        if(item != null) {
+            db.Dishes.Remove(item);
+            db.SaveChanges();
+            Console.WriteLine("Entry removed");
+        }
+        return RedirectToAction("Index");
+    }
+
 
     public IActionResult Privacy()
     {
